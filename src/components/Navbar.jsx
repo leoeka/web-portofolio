0a9812a1271ef286a321f1330a171471a851495f
leoeka <leoekamatra47770@gmail.com>
2026-06-04 09:06:55 +0700

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
     return (
         <nav className="sticky top-0 z-10 flex justify-between items-center px-8 py-4 bg-white shadow-sm">
             <div className="flex items-center gap-3 cursor-pointer" onClick={(e) => { e.preventDefault(); scrollToSection('hero'); }}>
-                <img src="./public/logo.jpeg" alt="Logo" className="h-10 w-10 rounded-full border-2 border-white" />
+                <img src="/logo.png" alt="Logo" className="h-10 w-10 rounded-full border-2 border-white" />
             <h1 className="text-xl md:text-2xl font-bold text-gray-900">Leo Eka Matra</h1>
             </div>
             <ul className="hidden md:flex gap-6 font-medium text-gray-600">
